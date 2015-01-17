3e9952cf684c5c906b27f3cbb9d09c8fe8a5a017
virtuecai <virtuecai@163.com>
2015-01-18 02:50:42 +0800

    copy maven-db-plugin into local project

--- bin/install.bat
+++ bin/install.bat
@@ -9,11 +9,15 @@ cd %~dp0
 cd ../parent
 call mvn clean install -pl . -Dmaven.test.skip=true
 
-
 echo [INFO] Install common.
 cd %~dp0
 cd ../common
 call mvn clean install -pl .  -Dmaven.test.skip=true
 
-cd ../bin
+echo [INFO] Install maven-plugins/maven-db-plugin.
+cd %~dp0
+cd ../maven-plugins/maven-db-plugin
+call mvn clean install -pl .  -Dmaven.test.skip=true
+
+cd ../../bin
 pause
